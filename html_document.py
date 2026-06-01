77a244bd6a47bde7a6184edcbce551f8dc67a9b7
Avery <averywoodlief@gmail.com>
2026-06-01 18:27:11 -0500

    update

--- html_document.py
+++ html_document.py
@@ -23,24 +23,25 @@ class Document:
 
 
 
-doc = Document("test")
+def example1():
+    doc = Document("example1")
 
-a1 = A()
-a1["href"] = "#1"
-a1['id']='a1'
-a2 = A()
-a2["href"] = "#2"
-a2["id"] = "a2"
+    a1 = A()
+    a1["href"] = "#1"
+    a1['id']='a1'
+    a2 = A()
+    a2["href"] = "#2"
+    a2["id"] = "a2"
 
-p = P()
-p["title"] = "short sentence"
-p.text = "This is a demo for p tag"
-p["id"] = "p1"
-a1.add(p)
+    p = P()
+    p["title"] = "short sentence"
+    p.text = "This is a demo for p tag"
+    p["id"] = "p1"
+    a1.add(p)
 
 
-doc.add(a1)
-doc.add(a2)
+    doc.add(a1)
+    doc.add(a2)
 
-print(doc.expand())
-doc.upload()
+    #print(doc.expand())
+    doc.upload()
